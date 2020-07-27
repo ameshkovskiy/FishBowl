4486c092a4dc4b7a3b2caea641edf71f9f1a07fa
ameshkovskiy <arturs.meskovskis@gmail.com>
2020-07-27 20:52:03 +0100

    Update test_topology.py

--- tests/test_topology.py
+++ tests/test_topology.py
@@ -9,7 +9,7 @@ class TestTopology:
         coord = SquareGridCoordinate(1, 2)
         assert coord.x == 1
         assert coord.y == 2
-        moved = coord.move(1, 1)
+        moved = coord.move(1, 1, grid_size=10) # AM: add grid_size as new argument
         assert moved.x == 2
         assert moved.y == 3
 
@@ -34,7 +34,7 @@ class TestTopology:
         assert len(neigh_list) == 8
         # corner
         neigh_list = square_grid_neighbours(10, SquareGridCoordinate(9, 9))
-        assert len(neigh_list) == 3
+        assert len(neigh_list) == 8 # Used to work fine. Changed from 3, since now we have 'infinite' grid
         # line
         neigh_list = square_grid_neighbours(10, SquareGridCoordinate(0, 5))
-        assert len(neigh_list) == 5
+        assert len(neigh_list) == 8 # Used to work fine. Changed from 5, since now we have 'infinite' grid
